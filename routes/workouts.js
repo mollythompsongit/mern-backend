3a4ed0dd4a7bf30c3838935cd25f71bc6476d4d6
mollythompsongit <mollythompsonxox@gmail.com>
2023-04-29 17:00:12 +1200

    push

--- routes/workouts.js
+++ routes/workouts.js
@@ -13,7 +13,7 @@ const router = express.Router()
 // this is to get all workouts
 
 
-router.get('/', getWorkouts)
+router.get('https://mern-backend-woad.vercel.app/', getWorkouts)
 // router.get('/', function (req, res) {
 //     console.log(req.hostname);
 //     () => getWorkouts
@@ -22,22 +22,22 @@ router.get('/', getWorkouts)
 
 
 // GET a single workout
-router.get('/:id', getWorkout)
+router.get('https://mern-backend-woad.vercel.app/:id', getWorkout)
 // router.get('/:id', function (req, res) {
 //     getWorkout
 // })
 
 // POST a new workout
-router.post('/', createWorkout)
+router.post('https://mern-backend-woad.vercel.app/', createWorkout)
 // router.post('/', function (req, res) {
 //     createWorkout
 // })
 
 // DELETE a workout
-router.delete('/:id', deleteWorkout)
+router.delete('https://mern-backend-woad.vercel.app/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', updateWorkout)
+router.patch('https://mern-backend-woad.vercel.app/:id', updateWorkout)
 // router.patch('/:id', function (req, res) {
 //     updateWorkout
 // })
