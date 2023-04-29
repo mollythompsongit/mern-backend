80c4ad026f0590f17fc6f72d28c361bd88c28121
mollythompsongit <mollythompsonxox@gmail.com>
2023-04-30 10:10:32 +1200

    url

--- server.js
+++ server.js
@@ -11,15 +11,24 @@ const mongoose = require('mongoose')
 //middleware
 app.use(express.json())
 
-// app.use((req, res, next) => {
-//     console.log(req.path, req.method)
-// })
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 
 // route handler
 app.get('/', (req, res) => {
     res.json({ msg: "Welcome to the app" })
 })
-app.use('/api/workouts', workoutRoutes)
+app.use('https://mern-backend-woad.vercel.app/api/workouts', workoutRoutes)
 
 // connect to DB
 mongoose.connect(process.env.MONG_URI)
